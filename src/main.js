f2e865e0fbb2907387188feddaac7fb0a1748f61
tugbabostanci <ttugbabostanci@gmail.com>
2025-11-02 14:41:43 +0300

    revise

--- src/main.js
+++ src/main.js
@@ -1,16 +1,1 @@
-import { loadConfigFromFile } from "vite";
-
-document.addEventListener('DOMContentLoaded', () => {
-  const galleryLink = document.querySelector('a[href="/01-gallery.html"]');
-  const formLink = document.querySelector('a[href="/02-form.html"]');
-  
-  galleryLink.addEventListener('click', (e) => {
-    console.log("Redirecting to Gallery");
-  });
-  
-  formLink.addEventListener('click', (e) => {
-    console.log("Redirecting to User Form");
-  });
-
-});
 
